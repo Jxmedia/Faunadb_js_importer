efdea946d3bd9e53a5cda4fb22944fd28eb134fe
unknown <josh@thejxmediagroup.com>
2021-05-05 19:39:54 -0400

    pretty much done

--- main.js
+++ main.js
@@ -1,105 +1,90 @@
 // FaunaDB Javascript Importer //
+// This tool/service should not be used in a production env //
 // Follow the steps below to import your data //
-
+//
 const FaunadbImporter = () => {
   const faunadb = require("faunadb");
   const q = faunadb.query;
-
+  //
+  // Step 01 - Input your FaunaDB Database Secret //
+  //
   const client = new faunadb.Client({ secret: process.env.GATSBY_FAUNA_DB });
-
-  const COLLECTION_NAME = process.env.GATSBY_FAUNA_COLLECTION;
-
+  //
+  // Step 02 - Input your formatted data from Mr Data Converter so that each entry or potential document is an array //
+  //
   const dataInputArray = [
     [
-      "1381",
-      "ZUNIGA MIGUEL",
-      "1503 DEREK DR",
-      "OLIVEHURST",
-      "CA",
-      "95961",
-      "YUBA",
-      "cynthia_zuniga@ymail.com",
+      "First Value in First Array",
+      "Second Value in First Array",
+      "Third Value in First Array",
+    ],
+    [
+      "First Value in Second Array",
+      "Second Value in Second Array",
+      "Third Value in Second Array",
     ],
   ];
-
-  // Create Documents in Fauna based on each item in Data Input Array //
-
-  const createUserImport = async () => {
-    for (var i = 0; i < dataInputArray2.length; i++) {
+  //
+  // For larger data arrays you may want to consider splitting them up //
+  //
+  /* const dataInputArrayAdditional = [
+    [
+      "First Value in First Additional Array",
+      "Second Value in First Additional Array",
+      "Third Value in First Additional Array",
+    ],
+  ];*/
+  //
+  // Create Documents in Fauna based on each array in dataInputArray //
+  //
+  const createDocumentImport = async () => {
+    for (var i = 0; i < dataInputArray.length; i++) {
       client.query(
-        q.Create(q.Collection("customers"), {
+        q.Create(q.Collection("Fauna DB Collection Name"), {
           data: {
-            fullName: dataInputArray2[i][1],
-            referralCode: dataInputArray2[i][0],
-            address: dataInputArray2[i][2],
-            city: dataInputArray2[i][3],
-            zip: dataInputArray2[i][5],
-            email: dataInputArray2[i][7],
+            First_key: dataInputArray[i][0],
+            Second_key: dataInputArray[i][1],
+            Third_key: dataInputArray[i][2],
           },
         })
       );
     }
   };
-
-  const createUserImport2 = async () => {
-    for (var i = 0; i < dataInputArray3.length; i++) {
+  //
+  // Import Function for dataInputArrayAdditional if needed  //
+  //
+  /*
+  const createDocumentImportAdditional = async () => {
+    for (var i = 0; i < dataInputArrayAdditional.length; i++) {
       client.query(
-        q.Create(q.Collection("customers"), {
+        q.Create(q.Collection("Fauna DB Collection Name"), {
           data: {
-            fullName: dataInputArray3[i][1],
-            referralCode: dataInputArray3[i][0],
-            address: dataInputArray3[i][2],
-            city: dataInputArray3[i][3],
-            zip: dataInputArray3[i][5],
-            email: dataInputArray3[i][7],
+            First_key: dataInputArrayAdditional[i][0],
+            Second_key: dataInputArrayAdditional[i][1],
+            Third_key: dataInputArrayAdditional[i][2],
+       ,
           },
         })
       );
     }
-  };
+  };*/
 
   return (
-    <main class="mt-10 mx-auto max-w-screen-xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20">
-      <div class="text-center">
-        <div className="mx-auto flex w-11/12">
-          <img
-            class=" w-full "
-            src="https://imgix.cosmicjs.com/803ccfa0-5ff4-11eb-ad8f-d1a7eae548e8-Header-Logo-01.svg"
-            alt="Logo"
-          ></img>
-        </div>
-        <h2 class="text-2xl md:text-4xl tracking-tight md:leading-12 font-extrabold text-blue sm:text-4xl sm:leading-none">
-          Refer a friend test page
-        </h2>
-        <button class="block" onClick={createUser}>
-          Create user here
-        </button>
-        <button class="block" onClick={getAllUsers}>
-          Get all users
-        </button>
-        <button class="block" onClick={getReferralCodes}>
-          Get all codes
-        </button>
-
-        <button class="block" onClick={searchByEmail}>
-          Search for code via email
-        </button>
-        <button class="block" onClick={searchByPostCard}>
-          Search for code via Ref Code
-        </button>
-        <button class="block" onClick={createUserImport}>
-          Try to import everything first half
-        </button>
-        <button class="block" onClick={createUserImport2}>
-          Try to import everything second half
-        </button>
-        <button class="block" onClick={() => setDataState({ name: "blah" })}>
-          Set name to blah3
-        </button>
+    <div>
+      <div>
+        <img
+          src="https://imgix.cosmicjs.com/803ccfa0-5ff4-11eb-ad8f-d1a7eae548e8-Header-Logo-01.svg"
+          alt="Logo"
+        ></img>
       </div>
-    </main>
+
+      <button onClick={createDocumentImport}>
+        Import everything in dataInputArray
+      </button>
+      <button onClick={createDocumentImportAdditional}>
+        Import everything in dataInputArrayAdditional if needed
+      </button>
+    </div>
   );
 };
 export default FaunadbImporter;
-
-/*      {createUser()}*/
